ebcdd6819495bf1a780eba767d3a69e6eae41591
omidbaharifar <weblax.ir@gmail.com>
2024-04-22 04:16:19 +0330

    feat: new ep added

---
+++ e27/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazirMatn: "Vazirmatn RD",
+      },
+    },
+    boxShadow: {
+      customShadow1: "0 0 15px -1px rgb(0 0 0 / 0.2)",
+    },
+  },
+  plugins: [],
+};
