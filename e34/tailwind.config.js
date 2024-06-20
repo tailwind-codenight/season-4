4e9d060c76b6cad1b649c7e3ac6226dc44ed6670
omidbaharifar <weblax.ir@gmail.com>
2024-06-20 05:50:21 +0330

    feat: new eps added

---
+++ e34/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        vazirMatn: "Vazirmatn RD",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
